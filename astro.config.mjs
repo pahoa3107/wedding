821e9d465731a89321591bcedd6241ee4df0f62a
haianh <vuhaianh1990@gmail.com>
2023-09-03 16:49:24 +0700

    feat(wedding): add tailwind, custom page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://vuhaianh1990.github.io',
+  base: '/wedding',
+  integrations: [tailwind()],
+});
